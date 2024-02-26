1ab01e9981597023f0510b379326af3de570d7ab
Lincoln <lincolninsper@gmail.com>
2024-02-26 09:33:33 -0300

    funcionando multiplicacao e div

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 # main.py
 import sys
 
-SOMA = 1
-SUB  = 0
-symbols = [SOMA, SUB]
+SUB  = 'SUB'
+SOMA = 'SOMA'
+MULT = 'MULT'
+DIV  = 'DIV'
+END = ''
+
+symbols = [SOMA, SUB, MULT, DIV]
 
 # Classes
 class Token():
@@ -40,6 +44,12 @@ class Tokenizer():
             elif self.source[self.position] == '-':
                 self.next = Token('MINUS', SUB)
                 self.position +=1
+            elif self.source[self.position] == '*':
+                self.next = Token('MULT', MULT)
+                self.position +=1
+            elif self.source[self.position] == '/':
+                self.next = Token('DIV', DIV)
+                self.position +=1
                 
         else:
             num = int(''.join(aux))
@@ -55,28 +65,76 @@ class Parser():
     # @staticmethod
     def parse_expression(self):
         res = 0 
+        expression_parts = []
+
         token_atual = self.tokenizer.select_next()
-        while token_atual.value  != '':
+        while token_atual.value  != END:
 
-            if token_atual.type == 'int' or token_atual.value in [SOMA,SUB,'']:
-                if (res == 0 and token_atual.type == 'int') :
-                    res = token_atual.value
-                elif token_atual.value == SOMA:
-                    token_atual = self.tokenizer.select_next()
-                    if token_atual.value in [SOMA,SUB]:
-                        raise "string com entrada invalida"
-
-                    res+= token_atual.value
-                elif token_atual.value == SUB:
-                    token_atual = self.tokenizer.select_next()
-                    if token_atual.value in [SOMA,SUB]:
-                        raise "string com entrada invalida"
-                    res-= token_atual.value
-                else:
-                    raise "string com entrada invalida"            
+            if token_atual.type == 'int' or token_atual.value in [SOMA,SUB,END,MULT,DIV]:
+                expression_parts.append(token_atual.value)            
+                # if (res == 0 and token_atual.type == 'int') :
+                #     res = token_atual.value
+                # elif token_atual.value == SOMA:
+                #     expression_parts.append(token_atual.value) 
+                #     token_atual = self.tokenizer.select_next()
+                #     if token_atual.value in [SOMA,SUB]:
+                #         raise "string com entrada invalida"
+                #     res+= token_atual.value
+                # elif token_atual.value == SUB:
+                #     expression_parts.append(token_atual.value) 
+                #     token_atual = self.tokenizer.select_next()
+                #     if token_atual.value in [SOMA,SUB]:
+                #         raise "string com entrada invalida"
+                #     res-= token_atual.value
+                # else:
+                #     raise "string com entrada invalida" 
+                # expression_parts.append(token_atual.value)           
             else:
                 raise "O valor do token não é um valor válido"
             token_atual = self.tokenizer.select_next()
+        
+        i = 0 
+        j = 0
+        aux = 0
+
+        while len(expression_parts) != 1:
+            print(i)
+            print(expression_parts)
+            if MULT in expression_parts or DIV in expression_parts:
+                if (expression_parts[i] == MULT):
+                    aux = expression_parts[i-1]*expression_parts[i+1]
+                    expression_parts.pop(i+1)
+                    expression_parts.pop(i)
+                    expression_parts[i-1] = aux
+                    i-=1
+
+                elif (expression_parts[i] == DIV):
+                    aux = expression_parts[i-1]/expression_parts[i+1]
+                    expression_parts.pop(i+1)
+                    expression_parts.pop(i)
+                    expression_parts[i-1] = aux
+                    i-=1
+            else:
+                if (expression_parts[j] == SOMA):
+                    aux = expression_parts[j-1]+expression_parts[j+1]
+                    expression_parts.pop(j+1)
+                    expression_parts.pop(j)
+                    expression_parts[j-1] = aux
+                    j-=1
+                elif (expression_parts[j] == SUB):
+                    aux = expression_parts[j-1]-expression_parts[j+1]
+                    expression_parts.pop(j+1)
+                    expression_parts.pop(j)
+                    expression_parts[j-1] = aux
+                    j-=1
+                j+=1
+            i+=1
+
+                
+
+        res = expression_parts[0]
+
+                         
         return res
         
     
@@ -90,13 +148,13 @@ class Parser():
 def main():
 
     # Verifica se há argumentos suficientes
-    if len(sys.argv) != 2:
-        print("Por favor, forneça uma string como argumento.")
-        return
+    # if len(sys.argv) != 2:
+    #     print("Por favor, forneça uma string como argumento.")
+    #     return
 
     # Obtém o argumento da linha de comando
-    minha_string = sys.argv[1]
-
+    # minha_string = sys.argv[1]
+    minha_string = '1 + 2 + 3 - 4 * 0'
     if minha_string[0] in symbols or minha_string[-1] in symbols:
         raise "Essa string é uma string invalida por não começar e/ou terminar com números"
 
