f816f76810bb017be370fe4069f070d915203e84
Lincoln <lincolninsper@gmail.com>
2024-02-27 08:59:11 -0300

    seguindo o padrão da disciplina

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # main.py
 import sys
 
-SUB  = 'SUB'
-SOMA = 'SOMA'
+SUB  = 'MINUS'
+SOMA = 'PLUS'
 MULT = 'MULT'
 DIV  = 'DIV'
 END = ''
@@ -21,14 +21,16 @@ class Token():
 
 
 class Tokenizer():
-    def __init__(self, source, position, next):
+    def __init__(self, source):
         self.source = source
-        self.position = position
-        self.next = next
+        self.position = 0
+        self.next = self.select_next()
 
+    # @staticmethod
     def select_next(self):
         if self.position >= len(self.source):
-            return Token("EOF", "")
+            self.next = Token("EOF", "")
+            return self.next
         while self.source[self.position] == ' ':
                     self.position +=1
         aux = []
@@ -60,116 +62,82 @@ class Tokenizer():
 
 class Parser():
     def __init__(self):
-        self.tokenizer = None
-    
-    # @staticmethod
-    def parse_expression(self):
-        res = 0 
-        expression_parts = []
+        Parser.tokenizer = None
 
-        token_atual = self.tokenizer.select_next()
-        while token_atual.value  != END:
+    tokenizer = None
 
-            if token_atual.type == 'int' or token_atual.value in [SOMA,SUB,END,MULT,DIV]:
-                # expression_parts.append(token_atual.value)            
-                if (res == 0 and token_atual.type == 'int') :
-                    expression_parts.append(token_atual.value) 
-                    # res = token_atual.value
-                elif token_atual.value in [SOMA,SUB,MULT,DIV]:
-                    expression_parts.append(token_atual.value) 
-                    token_atual = self.tokenizer.select_next()
-                    if token_atual.value in [SOMA,SUB]:
-                        raise "string com entrada invalida"
-                    expression_parts.append(token_atual.value) 
-                    
-                    # res+= token_atual.value
-                # elif token_atual.value == SUB:
-                #     expression_parts.append(token_atual.value) 
-                #     token_atual = self.tokenizer.select_next()
-                #     if token_atual.value in [SOMA,SUB]:
-                #         raise "string com entrada invalida"
-                #     expression_parts.append(token_atual.value) 
-                #     # res-= token_atual.value
+
+    @staticmethod
+    def parse_term():
+        res = Parser.tokenizer.next.value                            
+        Parser.tokenizer.select_next()
+        while Parser.tokenizer.next.type in [MULT, DIV]:
+            if Parser.tokenizer.next.type == 'int':
+                return Parser.tokenizer.next.value
+            elif Parser.tokenizer.next.type == MULT:
+                Parser.tokenizer.select_next()
+                if Parser.tokenizer.next.type == 'int':
+                    res *= Parser.tokenizer.next.value
                 else:
-                    raise "string com entrada invalida" 
-                # expression_parts.append(token_atual.value)           
-            else:
-                raise "O valor do token não é um valor válido"
-            token_atual = self.tokenizer.select_next()
-        
-        i = 0 
-        j = 0
-        aux = 0
+                    raise "tipo está errado"
+            elif Parser.tokenizer.next.type == DIV:
+                Parser.tokenizer.select_next()
+                if Parser.tokenizer.next.type == 'int':
+                    res = res // Parser.tokenizer.next.value
+                else:
+                    raise "tipo está errado"
+            Parser.tokenizer.select_next()
 
-        while len(expression_parts) != 1:
-            # print(i)
-            # print(expression_parts)
-            if MULT in expression_parts or DIV in expression_parts:
-                if (expression_parts[i] == MULT):
-                    aux = expression_parts[i-1]*expression_parts[i+1]
-                    expression_parts.pop(i+1)
-                    expression_parts.pop(i)
-                    expression_parts[i-1] = aux
-                    i-=1
+        return res
 
-                elif (expression_parts[i] == DIV):
-                    aux = expression_parts[i-1]/expression_parts[i+1]
-                    expression_parts.pop(i+1)
-                    expression_parts.pop(i)
-                    expression_parts[i-1] = aux
-                    i-=1
-            else:
-                if (expression_parts[j] == SOMA):
-                    aux = expression_parts[j-1]+expression_parts[j+1]
-                    expression_parts.pop(j+1)
-                    expression_parts.pop(j)
-                    expression_parts[j-1] = aux
-                    j-=1
-                elif (expression_parts[j] == SUB):
-                    aux = expression_parts[j-1]-expression_parts[j+1]
-                    expression_parts.pop(j+1)
-                    expression_parts.pop(j)
-                    expression_parts[j-1] = aux
-                    j-=1
-                j+=1
-            i+=1
+    
+    @staticmethod   
+    def parse_expression():
+        res = Parser.parse_term()
 
-                
-
-        res = int(expression_parts[0])
-
-                         
+        while Parser.tokenizer.next.value != END:
+            if Parser.tokenizer.next.type == SOMA:
+                Parser.tokenizer.select_next()
+                if Parser.tokenizer.next.type == 'int':
+                    res += Parser.parse_term()
+                else:
+                    raise "tipo está errado"
+            elif Parser.tokenizer.next.type == SUB:
+                Parser.tokenizer.select_next()
+                if Parser.tokenizer.next.type == 'int':
+                    res -= Parser.parse_term()
+                else:
+                    raise "tipo está errado"       
+            Parser.tokenizer.select_next()             
         return res
         
     
-    # @staticmethod
-    def run(self,code):
-        tokenizador= Tokenizer(code,0,None)
-        self.tokenizer = tokenizador
-        resultado = self.parse_expression()
+    @staticmethod
+    def run(code):
+        tokenizador= Tokenizer(code)
+        Parser.tokenizer = tokenizador
+        resultado = Parser.parse_expression()
         return resultado
     
 def main():
 
     # Verifica se há argumentos suficientes
-    if len(sys.argv) != 2:
-        print("Por favor, forneça uma string como argumento.")
-        return
+    # if len(sys.argv) != 2:
+    #     print("Por favor, forneça uma string como argumento.")
+    #     return
 
     #Obtém o argumento da linha de comando
-    minha_string = sys.argv[1]
+    # minha_string = sys.argv[1]
+    minha_string = "12/3*3+2"
+    
 
     if minha_string[0] in symbols or minha_string[-1] in symbols:
         raise "Essa string é uma string invalida por não começar e/ou terminar com números"
 
-    parsing = Parser()
-    res = parsing.run(minha_string)
+    res = Parser.run(minha_string)
     print(res)
 
             
 if __name__ == "__main__":
     main()
 
-
-
-"1/1   +  2 *  1 -3+4/2"
