28a96a44273c7f5d30993961c28f34ab8d244175
Lincoln <lincolninsper@gmail.com>
2024-05-16 11:58:27 -0300

    testing file

--- teste.lua
+++ teste.lua
@@ -1,3 +1,12 @@
-x = 15 + 12
-y = 16
-print(x + y)
+function soma(x , y)
+    local a
+    a = x + y
+    print(a)
+    return a
+    end
+local a
+local b
+a = 3
+b = soma(a, 4)
+print(a)
+print(b)
